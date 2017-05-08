899544c974eff7fde5c11928c94e270a38df71ef
Zackary Chapple <zackarychapple30@gmail.com>
2017-05-07 22:55:28 -0400

    feat(speakers): adding more speakers

--- src/app/pages/speakers/speakers.component.ts
+++ src/app/pages/speakers/speakers.component.ts
@@ -30,6 +30,110 @@ export class SpeakersComponent implements OnInit {
       social: {
         twitter: 'AysSomething'
       }
+    },
+    {
+      name: 'Tracy Lee',
+      image: '../assets/Tracy.jpg',
+      company: 'dotMedia',
+      social: {
+        twitter: 'ladyleet'
+      }
+    },
+    {
+      name: 'Carmen Popoviciu',
+      image: '../assets/Carmen.jpg',
+      company: '',
+      social: {
+        twitter: 'CarmenPopoviciu'
+      }
+    },
+    {
+      name: 'Tara Manicsic',
+      image: '../assets/Tara.jpg',
+      company: '',
+      social: {
+        twitter: 'Tzmanics'
+      }
+    },
+    {
+      name: 'Chaela Womack',
+      image: '../assets/Chaela.jpg',
+      company: 'CareerBuilder',
+      social: {
+        twitter: 'chawomack'
+      }
+    },
+    {
+      name: 'Sherry List',
+      image: '../assets/Sherry.jpg',
+      company: '',
+      social: {
+        twitter: 'sherrylist'
+      }
+    },
+    {
+      name: 'Patrick Stapleton',
+      image: '../assets/Patrick.jpg',
+      company: 'AngularClass',
+      social: {
+        twitter: 'gdi2290'
+      }
+    },
+    {
+      name: 'Vanessa Yuenn',
+      image: '../assets/Vanessa.jpg',
+      company: '',
+      social: {
+        twitter: 'vanessayuenn'
+      }
+    },
+    {
+      name: 'Kim Crayton',
+      image: '../assets/KimCrayton.jpg',
+      company: '',
+      social: {
+        twitter: 'KimCrayton1'
+      }
+    },
+    {
+      name: 'Shai Reznik',
+      image: '../assets/Shai.jpg',
+      company: '',
+      social: {
+        twitter: 'shai_reznik'
+      }
+    },
+    {
+      name: 'Samantha Lee',
+      image: '../assets/Samantha.jpg',
+      company: '',
+      social: {
+        twitter: 'SamLee_509'
+      }
+    },
+    {
+      name: 'April Wensel',
+      image: '../assets/April.jpg',
+      company: '',
+      social: {
+        twitter: 'aprilwensel'
+      }
+    },
+    {
+      name: 'Susan Goldblatt',
+      image: '../assets/Susan.jpg',
+      company: '',
+      social: {
+        twitter: 'thatgoldblatt'
+      }
+    },
+    {
+      name: 'Kapunahele Wang',
+      image: '../assets/Kapunahele.jpg',
+      company: '',
+      social: {
+        twitter: 'kapunahele'
+      }
     }
   ];
 
