9dc665597678c845f34ce72fbaf498d50eda0e84
Zackary Chapple <zackarychapple30@gmail.com>
2017-05-07 22:41:55 -0400

    feat(speakers): corrections for speaker layout

--- src/app/pages/speakers/speakers.component.ts
+++ src/app/pages/speakers/speakers.component.ts
@@ -18,6 +18,7 @@ export class SpeakersComponent implements OnInit {
     {
       name: 'Mike Brocchi',
       image: '../assets/Mike.jpg',
+      company: 'Rangle.io',
       social: {
         twitter: 'Brocco'
       }
@@ -25,6 +26,7 @@ export class SpeakersComponent implements OnInit {
     {
       name: 'Aysegul Yonet',
       image: '../assets/Aysegul.jpg',
+      company: 'Autodesk',
       social: {
         twitter: 'AysSomething'
       }
