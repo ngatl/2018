45adfa721efe45134f3937fd1419a532d5c45076
Bram Borggreve <borggreve@gmail.com>
2017-05-08 23:27:18 -0500

    fix(build): fix deploy setup and script

--- bin/deploy-gh-pages.sh
+++ bin/deploy-gh-pages.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+npm i --silent -g angular-cli-ghpages
+
 cp dist/index.html dist/404.html
 echo $GH_DOMAIN > dist/CNAME
-ngh angular-cli-ghpages --repo=https://GH_TOKEN@github.com/$GH_USER/$GH_REPO.git --name="Travis CI" --email=travis@example.com
+ngh angular-cli-ghpages --no-silent --repo=https://GH_TOKEN@github.com/$GH_USER/$GH_REPO.git --name="Travis CI" --email=travis@example.com
