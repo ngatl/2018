05afd5a386a511006b7baff39a947ccc5473ba72
Oleksandr <oleksandr.telnov@valor-software.com>
2017-07-27 14:56:44 +0300

    feat(universal): update universal build to work with new cli

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,23 @@ import 'zone.js/dist/zone-node';
 
 import { renderModuleFactory } from '@angular/platform-server'
 import { enableProdMode } from '@angular/core'
-import { AppServerModuleNgFactory } from '../dist/ngfactory/src/app/app.server.module.ngfactory'
 import * as express from 'express';
 import { readFileSync } from 'fs';
 import { join } from 'path';
+
+const fs = require('fs');
+const path = require('path');
+const distPath = '../dist-server/';
+const files = fs.readdirSync(path.resolve(__dirname, distPath));
+
+let bundle = '';
+for (const i in files) {
+  if (path.extname(files[i]) === '.js') {
+    bundle = distPath + files[i];
+  }
+}
+
+const AppServerModuleNgFactory = require(bundle).AppServerModuleNgFactory;
 
 const PORT = 4000;
 
@@ -14,10 +27,10 @@ enableProdMode();
 
 const app = express();
 
-const template = readFileSync(join(__dirname, '..', 'dist', 'index.html')).toString();
+const template = readFileSync(join(__dirname, '..', 'src', 'index.html'), 'utf8');
 
 app.engine('html', (_, options, callback) => {
-  const opts = { document: template, url: options.req.url };
+  const opts = {document: template, url: options.req.url};
 
   renderModuleFactory(AppServerModuleNgFactory, opts)
     .then(html => callback(null, html));
@@ -26,10 +39,10 @@ app.engine('html', (_, options, callback) => {
 app.set('view engine', 'html');
 app.set('views', 'src');
 
-app.get('*.*', express.static(join(__dirname, '..', 'dist')));
+app.get('*.*', express.static(join(__dirname, '..', 'dist-server')));
 
 app.get('*', (req, res) => {
-  res.render('index', { req });
+  res.render('index', {req});
 });
 
 app.listen(process.env.PORT || PORT, () => {
